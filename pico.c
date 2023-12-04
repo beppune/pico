2dc394bcb2afa8a935000e238acbbf4d5a24b923
beppune <manzo.giuseppe@gmail.com>
2023-12-04 20:54:52 +0100

    initial commit

--- pico.c
+++ pico.c
@@ -4,17 +4,17 @@
 #include <termios.h>
 #include <unistd.h>
 
-struct termios orig_termios;
+struct termios orig_term;
 
 void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term);
 }
 
 void enableRawMode() {
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	tcgetattr(STDIN_FILENO, &orig_term);
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = orig_term;
 	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
